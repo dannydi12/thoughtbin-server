ccca9bd3e0cc6c2b726db22b7aa10fcc27ddd87c
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-29 11:56:19 -0700

    trying again

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const ws = require('ws');
+const { Server } = require('ws');
 const knex = require('knex');
 const app = require('./app');
 const { PORT, WS_PORT, DATABASE_URL } = require('../config');
@@ -14,18 +14,18 @@ app.set('db', db);
 
 // create web socket instance
 const socketServer = http.createServer(app);
-const websocket = new ws.Server({ server: socketServer });
+const wss = new Server({ server: app });
 
-websocket.on('connection', (socket) => {
+wss.on('connection', (socket) => {
   // socket.send('hey');
 });
 
-app.set('websocket', websocket);
+app.set('websocket', wss);
 
 // servers eagerly listening
-socketServer.listen(PORT, () => {
-  console.log(`Socket is up at ws://localhost:${PORT}`);
-});
+// wss.listen(WS_PORT, () => {
+//   console.log(`Socket is up at ws://localhost:${WS_PORT}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
